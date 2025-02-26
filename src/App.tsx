6806f2999a24f936c63ff6a91aa5e95fc2095fde
Fé <epharrafe@yahoo.com>
2025-02-25 19:07:04 -0800

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CopilotKit } from "@copilotkit/react-core";
+import { CopilotSidebar } from "@copilotkit/react-ui";
+import "@copilotkit/react-ui/styles.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +21,16 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <CopilotKit publicApiKey="ck_pub_7cd8774cb7264979bbdfdc94cf0f1c3f">
+          <CopilotSidebar
+            labels={{
+              title: "Sidebar Assistant",
+              initial: "How can I help you today?"
+            }}
+            instructions="Your product deserves an AI sidekick"
+          />
+        </CopilotKit>
     </div>
   );
 }
